c00b538e7040f0b4d2a481113133952ca44857f9
damianooldoni <damiano.oldoni@inbo.be>
2022-06-20 16:21:22 +0200

    Add mapping

---
+++ src/dwc_mapping.R
@@ -0,0 +1,17 @@
+# Mapping file
+
+# read input data
+input_data <-readr::read_csv("./data/raw/rato_data.csv")
+
+# mapping
+event <- input_data
+event <- event %>%
+  dplyr::mutate(dwc_type = "Event") %>%
+  dplyr::mutate(dwc_language = "en") %>%
+  dplyr::mutate(dwc_license = "http://creativecommons.org/publicdomain/zero/1.0/") %>%
+  dplyr::mutate(dwc_datasetTitle = "Test dataset") %>%
+  dplyr::select(starts_with("dwc_"))
+
+# save output
+readr::write_csv("./data/processed/event.csv")
+
--- src/install_packages.R
+++ src/install_packages.R
@@ -1,6 +1,6 @@
 # install needed packages
 installed <- rownames(installed.packages())
-required <- c("httr", "ows4R", "here")
+required <- c("httr", "ows4R", "here", "readr", "dplyr")
 if (!all(required %in% installed)) {
   pkgs_to_install <- required[!required %in% installed]
   print(paste("Packages to install:", paste(pkgs_to_install, collapse = ", ")))
