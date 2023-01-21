110f87171581dd668220f95e0d764dfc03dca1de
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-01-22 00:26:10 +0100

    Check quote effect in var

--- src/download_via_webdav.bat
+++ src/download_via_webdav.bat
@@ -1,7 +1,8 @@
 echo off
 set arg1=%1
+echo %arg1%
 net use w: "https://int-web.vmm.be/ratten" %arg1% /user:INBO
 w:
-copy *.csv c:\Users\damiano_oldoni\Documents\GitHub\test-workflows\src
-c:
+copy *.csv d:\a\test-workflows\test-workflows
+d:
 net use w: /delete
